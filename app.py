87f2b6aeff0c2fb25c0730be600b2243344fb1e6
vinayak3620 <vinayakgoyal900@gmail.com>
2023-08-02 00:03:27 +0530

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask
+from flask import Flask,request,jsonify,render_template
+import pickle
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+## import reidge regressor model and standard scaler pickle file.
+
+ridge_model = pickle.load(open('Models/Algerian Forest Fire Model.pkl','rb'))
+standard_scaler_model = pickle.load(open('Models/Algerian Forest Fire Scaler.pkl','rb'))
+
+## Route for home page
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predictdata',methods = ['GET','POST'])
+def predict_datapoint():
+    if request.method == 'POST':
+        Temperature = float(request.form.get('Temperature'))
+        RH = float(request.form.get('RH'))
+        Ws = float(request.form.get('Ws'))
+        Rain = float(request.form.get('Rain'))
+        FFMC = float(request.form.get('FFMC'))
+        DMC = float(request.form.get('DMC'))
+        ISI = float(request.form.get('ISI'))
+        Classes = float(request.form.get('Classes'))
+        Region = float(request.form.get('Region'))
+
+        new_scaled_data = standard_scaler_model.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
+        result = ridge_model.predict(new_scaled_data)
+
+        return render_template('home.html',result = result[0])
+    else:
+        return render_template('home.html')
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
+
+
